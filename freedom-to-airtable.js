3e1b49e71255fbe8e6ebe375550520f480f123ee
Alex Andrews <alex@alexandrews.info>
2020-03-16 15:45:37 +0000

    Refactor to use new tools

--- freedom-to-airtable.js
+++ freedom-to-airtable.js
@@ -4,9 +4,13 @@ const cheerio = require("cheerio");
 const _ = require("lodash/fp");
 const normalizeUrlL = require("normalize-url");
 
-const { toFacebookDesktop, toWWWFacebook } = require("./src/urls.js");
-
-const base = require("airtable").base("appo0BT91w2rrr856");
+const {
+  toFacebookDesktop,
+  toWWWFacebook,
+  normaliseUrl
+} = require("./src/urls.js");
+const { getGroupsFromAirtable, chunkForAirtable } = require("./src/airtable");
+const { findMissingGroups } = require("./src/lists");
 
 const FREEDOM_LIST =
   "https://freedomnews.org.uk/covid-19-uk-mutual-aid-groups-a-list/";
@@ -22,23 +26,7 @@ const nationalLinks = [
   "https://queercare.network/covid-19/"
 ];
 
-const chunkForAirtable = _.chunk(10);
-
 const dropNational = _.remove(href => nationalLinks.includes(href));
-
-const normaliseUrl = href => {
-  return toWWWFacebook(
-    toFacebookDesktop(
-      normalizeUrlL(href, {
-        forceHttps: true,
-        stripHash: true,
-        stripWWW: false,
-        removeQueryParameters: [/(.*?)/],
-        removeTrailingSlash: true
-      })
-    )
-  );
-};
 
 const removeSearchFromUrl = _.map(normaliseUrl);
 
@@ -54,72 +42,54 @@ async function getFreedomLinks() {
     rawLinks.push($(link).attr("href"));
   });
 
-  return dropNational(removeSearchFromUrl(rawLinks));
+  return dropNational(removeSearchFromUrl(rawLinks)).map(rawLink => ({
+    url: rawLink
+  }));
 }
 
 (async () => {
   const freedomLinks = await getFreedomLinks();
 
-  const groupsFromAirtable = [];
+  const groupsFromAirtable = await getGroupsFromAirtable();
 
-  await base("COVID-19 UK Mutual Aid Groups")
-    .select({
-      view: "All Groups"
-    })
-    .eachPage((records, fetchNextPage) => {
-      records.forEach(function(record) {
-        const facebookGroup = record.get("Facebook Group");
-        groupsFromAirtable.push({
-          id: record.id,
-          url: normaliseUrl(record.get("Facebook Group")),
-          name: record.get("Name"),
-          location: record.get("Location")
-        });
-      });
-      fetchNextPage();
-    });
+  const notFoundInFreedomList = findMissingGroups(
+    groupsFromAirtable,
+    freedomLinks
+  );
 
-  const notFoundInFreedomList = [];
-  const inFreedomList = [];
-
-  groupsFromAirtable.forEach(group => {
-    if (!freedomLinks.includes(group.url)) {
-      notFoundInFreedomList.push(group);
-      return;
-    }
-    inFreedomList.push(group);
-  });
+  // Get groups actually in Freedom list so we can mark them
 
   console.log(`Freedom list has ${freedomLinks.length} groups`);
   console.log(`${groupsFromAirtable.length} groups in Airtable`);
   console.log(`${notFoundInFreedomList.length} not found in Freedom list`);
-  console.log("Updating Freedom syncing");
+
+  // console.log("Updating Freedom syncing");
 
   // Ones missing from main list but in Freedom list
 
-  const chunkedForAirtableMissing = chunkForAirtable(
-    notFoundInFreedomList.map(group => ({
-      id: group.id,
-      fields: {
-        "Including On Freedom List": false
-      }
-    }))
-  );
+  // const chunkedForAirtableMissing = chunkForAirtable(
+  //   notFoundInFreedomList.map(group => ({
+  //     id: group.id,
+  //     fields: {
+  //       "Including On Freedom List": false
+  //     }
+  //   }))
+  // );
 
-  chunkedForAirtableMissing.forEach(async chunk => {
-    const results = await base("COVID-19 UK Mutual Aid Groups").update(chunk);
-  });
+  // chunkedForAirtableMissing.forEach(async chunk => {
+  //   const results = await base("COVID-19 UK Mutual Aid Groups").update(chunk);
+  // });
 
-  const chunkedForAirtablePresent = chunkForAirtable(
-    inFreedomList.map(group => ({
-      id: group.id,
-      fields: {
-        "Including On Freedom List": true
-      }
-    }))
-  );
+  // const chunkedForAirtablePresent = chunkForAirtable(
+  //   inFreedomList.map(group => ({
+  //     id: group.id,
+  //     fields: {
+  //       "Including On Freedom List": true
+  //     }
+  //   }))
+  // );
 
-  chunkedForAirtablePresent.forEach(async chunk => {
-    const results = await base("COVID-19 UK Mutual Aid Groups").update(chunk);
-  });
+  // chunkedForAirtablePresent.forEach(async chunk => {
+  //   const results = await base("COVID-19 UK Mutual Aid Groups").update(chunk);
+  // });
 })();
